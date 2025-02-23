1f7fb7a040b67a7c3947e855471da017950e670a
objones25 <59661118+objones25@users.noreply.github.com>
2025-02-23 09:49:44 -0800

    Fix 404 page and routing issues

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/Website/' : '/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
